9d376bb9bb01c00f7db972aed9da5e8cff055834
battulaakash <battulaakash2003@gmail.com>
2026-07-25 02:01:28 +0530

    Initialize React recruitment frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Career Connect</h1>
+      <p>Web-Based Recruitment System</p>
+    </div>
+  );
+}
+
+export default App;
