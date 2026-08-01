5822b918471bdd1d86795b569cb59759feecada9
Abhijeet Arekar <abhijeetarekar8775@gmail.com>
2026-08-01 16:18:54 +0530

    feat(offer): add offer entity and repository

---
+++ talentbridge-backend/src/main/java/com/talentbridge/repository/OfferRepository.java
@@ -0,0 +1,12 @@
+package com.talentbridge.repository;
+
+import com.talentbridge.entity.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+	Optional<Offer> findByApplicationId(Long applicationId);
+
+}
